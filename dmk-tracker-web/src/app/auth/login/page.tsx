bb463cfa0f645cbe86d84aead75cd7aee351d75a
Tuf\Asus <leeclementyf@hotmail.com>
2026-02-19 21:37:35 +0800

    [Session Expired]

--- dmk-tracker-web/src/app/auth/login/page.tsx
+++ dmk-tracker-web/src/app/auth/login/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { useState, useEffect } from "react";
-import { useRouter, useSearchParams  } from "next/navigation";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 import { EyeIcon, EyeSlashIcon } from "@heroicons/react/24/outline";
 
 export default function LoginPage() {
